518906595f3c971929879fba0588e8b25c8bacde
azharjk <azhr.project@gmail.com>
2022-05-15 09:01:06 +0700

    Add showcase page

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from "express";
 
+const HOST = '0.0.0.0';
 const PORT = 5000;
+
 const app = express();
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req: Request, res: Response) => {
   res.render('pages/index');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is listening on host ${HOST} and port ${PORT}`);
 });
