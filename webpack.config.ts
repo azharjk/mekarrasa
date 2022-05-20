fd210d9cc38af0676f33d1f224f161d57c2bc68f
azharjk <azhr.project@gmail.com>
2022-05-20 18:31:40 +0700

    Add react

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,26 +1,34 @@
-import path from 'path';
-import { Configuration } from 'webpack';
+import path from "path";
+import { Configuration } from "webpack";
 
 const config: Configuration = {
-  mode: 'development',
+  mode: "development",
   watch: true,
-  entry: './app/index.ts',
+  entry: "./app/index.tsx",
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'public/dist/')
+    filename: "app.js",
+    path: path.resolve(__dirname, "public/dist/"),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  }
+    extensions: [".tsx", ".ts", ".js"],
+  },
 };
 
 module.exports = config;
